2a45f3efc2819eb49e69cd6391c466fd6032fd1b
kayden van wely <38343@ma-web.nl>
2024-11-29 10:09:15 +0100

    E

--- functions/Program.cs
+++ functions/Program.cs
@@ -1,15 +1,31 @@
 ﻿namespace functions
-{
+{   
     internal class Program
     {
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.run();//we hebben een program gemaakt met de naam program.
+            program.Run();//we hebben een program gemaakt met de naam program.
+
+            Program vraag1 = new Program();
+            program.vraag1();
         }
-        internal void run()
+        internal void Run()
         {
-            Console.WriteLine("Dit is nu de start van mijn programma");
+            Console.WriteLine("...");
         }    
+        internal void vraag1()
+        {
+            Console.WriteLine("how long would you survive in a zombie apocolips?");
+            var words = new[] { "idk dont ask me", "a weak", "you will not survive i will" };
+            var wordsInRandomOrder = words.OrderBy(i => Guid.NewGuid());
+
+            foreach (var word in wordsInRandomOrder)
+            {
+                Console.WriteLine(word);
+            }
+
+
+        }
     }
 }
